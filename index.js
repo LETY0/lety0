720ba6b5b145ab877f9bbce82d6b9f31e870373d
LETY0 <138343821+LETY0@users.noreply.github.com>
2023-07-01 20:37:50 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const yesBtn = document.querySelector('#yesBtn');
+
+yesBtn.addEventListener('click',function () {
+    alert('sabia que dirias que si,  aunque intentastes poner no jaja. eres unico lo sabias, tienes una bella sonrisa')
+});
+
+const noBtn = document.querySelector('#noBtn');
+
+noBtn.addEventListener('mouseover', function () {
+    const randomX = parseInt(Math.random()*100);
+    const randomY = parseInt(Math.random()*100);
+    noBtn.style.setProperty('top',randomY+'%');
+    noBtn.style.setProperty('left',randomX+'%');
+    noBtn.style.setProperty('transform',`translate(-${randomX}%,-${randomY}%)`);
+})
+
